31757eca8b5c7f32668db77fd86cbf186b896b29
unknown <nataliematos12@gmail.com>
2023-03-30 14:17:04 -0400

    initial

---
+++ index.js
@@ -0,0 +1,48 @@
+// const bob = {
+//     name: 'Bob',
+//     age: 99
+// };
+
+// const greg = {
+//     name: 'Greg',
+//     age: 29
+// };
+
+// const Megan = {
+//     name: 'Megan',
+//     age: 32
+// };
+
+// function User(user_name, age) {
+//     this.name = user_name;
+//     this.age = age
+//     this.printData = function () {
+//         console.log('Username: ' + this.name);
+//         console.log('Age: ' + this.age);
+//     }
+// };
+
+// class User {
+//     constructor(user_name, age) {
+//         this.name = user_name;
+//         this.age = age
+//     }
+//     printData() {
+//         console.log('Username: ' + this.name);
+//         console.log('Age: ' + this.age);
+          
+//     }
+// };
+
+// const jd = new User('JD', 43);
+// const bob = new User('Bob', 99);
+// const greg = new User('Greg', 29);
+
+// jd.name = 'JD';
+// console.log(jd, bob, greg);
+
+// jd.printData();
+
+
+
+
